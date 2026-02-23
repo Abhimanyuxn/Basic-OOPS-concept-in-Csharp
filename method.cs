afa102a9d18c23da2d947d10ec9f40c1cd88c64d
Abhimanyu <abhimanyu21chauhan@gmail.com>
2026-02-23 22:25:30 +0530

    Add files via upload

---
+++ method.cs
@@ -0,0 +1,14 @@
+using System;
+class method
+{
+    static void Main(string[]args)
+
+    {
+        hill();
+    }
+    static void hill()
+    {
+        Console.WriteLine("you won");
+        Console.WriteLine("you won");
+    }
+}
